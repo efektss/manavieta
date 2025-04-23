732fb090dbd17aafb101baef12a766db156edccf
SHITONALDO <134733292+efektss@users.noreply.github.com>
2025-04-23 10:48:45 +0300

    Update uzdevums.py

--- uzdevums.py
+++ uzdevums.py
@@ -1,1 +1,11 @@
-
+skaitlis= int (input("Ievadi skaitli"))
+if skaitlis > 0:
+    print ("skaitlis pozitīvs")
+elif skaitlis == 0:
+    print ("skaitlis ir nulle")
+elif skaitlis < 0:
+    print("skaitlis negatīvs")
+if skaitlis %2==0:
+    print("skaitlis ir pāra")
+else: 
+    print("skaitlsi ir nepāra")
